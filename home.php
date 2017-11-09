e38ede9c89a520e9685dae275121e16ff5a36358
sjadamem <31553649+sjadamem@users.noreply.github.com>
2017-11-09 14:58:54 -0800

    Everything is .php
    its all .php lol

--- home.php
+++ home.php
@@ -15,55 +15,7 @@
     </head>
     <body>
         <!-- Fixed navbar -->
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">BeachBooks</a>
-                </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li><a href="home.html">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li class="dropdown-header">Nav header</li>
-                                <li><a href="#">Separated link</a></li>
-                                <li><a href="#">One more separated link</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <!-- Searchbar to be added -->
-                    
-                    <!-- TODO: Refresh page whilst signed in -->
-                    <form class="navbar-form navbar-right">
-                        <div class="form-group">
-                            <input type="text" placeholder="Email" class="form-control circular">
-                        </div>
-                        <div class="form-group">
-                            <input type="password" placeholder="Password" class="form-control circular">
-                        </div>
-                        <button type="submit" class="btn btn-success">Sign in</button>
-
-                        <!-- TODO: Link to Sign Up Page -->
-                        <form class="navbar-form navbar-right">
-                            <button type="submit" class="btn btn-success">Sign up!</button>
-                        </form>
-                    </form>
-                </div><!--/.nav-collapse -->
-            </div>
-        </nav>
+        <?php include('nav.php'); ?>
 
         <div class="container">
             <table class="table table-bordered" id="bookList">
---
+++ negotiate.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link href="css/starter-template.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        <title>BeachBooks - Sign Up</title>
+
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <!-- Fixed navbar -->
+        <?php include('nav.php'); ?>
+        <div class="col-md-12">
+            <h1 class="form-signin-heading" style="text-align:center;">NEGOTIATE</h1>
+        </div>
+    </body>
+</html>
