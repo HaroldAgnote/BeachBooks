d2a153d3b823699c365a44c0d1ff15a8b28b85c1
Harold Agnote <haroldagnote38@hotmail.com>
2017-11-11 12:25:13 -0800

    Linking content to modal

--- js/loadBookModal.js
+++ js/loadBookModal.js
@@ -1,8 +1,15 @@
-var books = document.getElementsByClassName("bookEntry");
-for (var i = 0; i < books.length; i++) {
-    book = books[i];
-    bookInfo = book.childNodes;
-    book.onclick = function() {
-        console.log(bookInfo[3].innerHTML); 
-    }
-}
+$(".bookEntry").click(function () {
+    var id = this.id;
+    var book = document.getElementById(id)
+    var bookSeller = book.childNodes[1].innerHTML;
+    var bookTitle = book.childNodes[5].innerHTML;
+    var bookAuthor = book.childNodes[7].innerHTML;
+    var bookEdition = book.childNodes[9].innerHTML;
+    console.log(id);
+    console.log(bookTitle);
+    
+    document.getElementById("bookModalTitle").innerHTML = "Book Title: " + bookTitle; 
+    document.getElementById("bookModalAuthor").innerHTML = "Book Author: " + bookAuthor; 
+    document.getElementById("bookModalEdition").innerHTML = "Book Edition " + bookEdition; 
+    document.getElementById("bookModalSeller").innerHTML = "Book Seller: " + bookSeller; 
+});
