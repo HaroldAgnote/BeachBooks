26e49051aad933660a6623de2bab7bef715fc07d
sjadamem <31553649+sjadamem@users.noreply.github.com>
2017-11-14 16:19:42 -0800

    Lots of changes
    Implemented the privacy modal for the appointment.php. Click the link on the checkbox to access the modal.
    In request.php, implemented an alert for approving an appointment time set by the seller through a button. Also for requests by buyer the seller can press a button to schedule an appointment time and day (links to appointment.php).
    Started the about.php and privacty.php but the about page div won't turn into a scroll.
    
    Be cautious! There were changes to the database's css files that I didn't do.  I'm not sure what happened. Please check.

--- request.php
+++ request.php
@@ -22,48 +22,105 @@ on YouTube when you are logged in -->
         <?php include('nav.php'); ?>
 
         <div class="container center-block row">
+            <!-- Implement an alert that shows up in front of the page when the buyer clicks the 'Agree to Appointment' button-->
             <div class="col-md-6">
-                <h1 class="form-signin-heading" style="text-align:center;">BUYER REQUESTS</h1>
+                <h1 class="form-signin-heading" style="text-align:center;">YOUR REQUESTS</h1>
                 <div class="panel-group" id="accordion">
                     <div class="panel panel-primary">
                         <div class="panel-heading">
-                          <h4 class="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Book A</a>
-                          </h4>
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#yourReq1">Book A</a>
+                            </h4>
                         </div>
-                        <div id="collapse1" class="panel-collapse collapse">
-                          <div class="panel-body"><h4>Request Info 1</h4></div>
+                        <div id="yourReq1" class="panel-collapse collapse">
+                            <div class="panel-body"><h4>Book Info 1</h4></div>
                         </div>
-                        <div class="panel-footer">Requested by: <b><i>Username</b></i></div>
+                        <div class="panel-footer">Requesting from: <b><i>Username</b></i>
+                            <a class="btn btn-sm btn-success" role="button" href="appointment.php">Agree to Appointment</a>
+                        </div>
                     </div>
                     <div class="panel panel-primary">
                         <div class="panel-heading">
                             <h4 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Book B</a>
+                                <a data-toggle="collapse" data-parent="#accordion" href="#yourReq2">Book B</a>
                             </h4>
                         </div>
-                        <div id="collapse2" class="panel-collapse collapse">
-                          <div class="panel-body"><h4>Request Info 2</h4></div>
+                        <div id="yourReq2" class="panel-collapse collapse">
+                            <div class="panel-body"><h4>Book Info 2</h4></div>
                         </div>
-                        <div class="panel-footer">Requested by: <b><i>Username</b></i></div>
+                        <div class="panel-footer">Requesting from: <b><i>Username</b></i>
+                            <a class="btn btn-sm btn-success" role="button" href="appointment.php">Agree to Appointment</a>
+                        </div>
                     </div>
                     <div class="panel panel-primary">
                         <div class="panel-heading">
                             <h4 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">Book C</a>
+                                <a data-toggle="collapse" data-parent="#accordion" href="#yourReq3">Book C</a>
                             </h4>
                         </div>
-                        <div id="collapse3" class="panel-collapse collapse">
-                            <div class="panel-body"><h4>Request Info 3</h4></div>
+                        <div id="yourReq3" class="panel-collapse collapse">
+                            <div class="panel-body"><h4>Book Info 3</h4></div>
                         </div>
-                        <div class="panel-footer">Requested by: <b><i>Username</b></i></div>
+                        <div class="panel-footer">Requesting from: <b><i>Username</b></i>
+                            <a class="btn btn-sm btn-success" role="button" href="appointment.php">Agree to Appointment</a>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-6">
-
+                <h1 class="form-signin-heading" style="text-align:center;">BUYER REQUESTS</h1>
+                <div class="panel-group" id="accordion2">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                          <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion2" href="#buyerReq1">Book A</a>
+                          </h4>
+                        </div>
+                        <div id="buyerReq1" class="panel-collapse collapse">
+                          <div class="panel-body"><h4>Location Info 1</h4></div>
+                        </div>
+                        <div class="panel-footer">Requested by: <b><i>Username</b></i>
+                            <a class="btn btn-sm btn-success" role="button" href="appointment.php">Set an Appointment</a>
+                        </div>
+                    </div>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion2" href="#buyerReq2">Book B</a>
+                            </h4>
+                        </div>
+                        <div id="buyerReq2" class="panel-collapse collapse">
+                          <div class="panel-body"><h4>Location Info 2</h4></div>
+                        </div>
+                        <div class="panel-footer">Requested by: <b><i>Username</b></i>
+                            <a class="btn btn-sm btn-success" role="button" href="appointment.php">Set an Appointment</a>
+                        </div>
+                    </div>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion2" href="#buyerReq3">Book C</a>
+                            </h4>
+                        </div>
+                        <div id="buyerReq3" class="panel-collapse collapse">
+                            <div class="panel-body"><h4>Location Info 3</h4></div>
+                        </div>
+                        <div class="panel-footer">Requested by: <b><i>Username</b></i>
+                            <a class="btn btn-sm btn-success" role="button" href="appointment.php">Set an Appointment</a>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+
+        <!-- Alert for agreeing to an appointment location and date set by a seller -->
+        <div class="container">
+            <div class="alert alert-success fade out" id="approveAppoint">
+              <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                  Approval for the location and date of the appointment has been sent to the buyer.
+            </div>
+        </div>
 
         <script src="js/jquery-3.2.1.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
--- signup.php
+++ signup.php
@@ -40,17 +40,43 @@
                     <label for="inputFirst" class="sr-only">First Name</label>
                     <input type="text" id="inputFirst" class="form-control"
                     placeholder="First Name" required autofocus>
-                    <label for="inputLast" class="sr-only">Last</label>
+                    <label for="inputLast" class="sr-only">Last Name</label>
                     <input type="text" id="inputLast" class="form-control"
                     placeholder="Last Name" required autofocus>
                     <label for="inputEmail" class="sr-only">Email address</label>
                     <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
                     <label for="inputPassword" class="sr-only">Password</label>
                     <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="terms" value="agree"> By clicking this you agree to BeachBooks's
+                        <a data-toggle="modal" data-target="#termsModal"><strong>Terms of Service</strong></a></label>
+                    <div class="checkbox">
                     <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
                 </form>
             </div>
 
         </div> <!-- /container -->
+
+        <div class="container">
+            <div class="modal fade" id="termsModal" role="dialog">
+                <div class="modal-dialog">
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                      <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          <h4>Terms of Service</h4>
+                      </div>
+                      <div class="modal-body">
+                          <p><!-- Terms of Condition --></p>
+                      </div>
+                      <div class="modal-footer">
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
