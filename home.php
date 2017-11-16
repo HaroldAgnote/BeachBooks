fc29b5736e0bfa8b5ec83588e17e62c2dd7f32c0
Harold Agnote <haroldagnote38@hotmail.com>
2017-11-15 16:33:29 -0800

    Organizing some stuff; preparing for search

--- home.php
+++ home.php
@@ -43,16 +43,21 @@
                     <th>Condition</th>
                 </tr>
                 <?php 
-                    $query = " SELECT clientName, Title, Author, Edition,(COALESCE(cl.subjectname, '') || COALESCE(cl.subjectnum, '')) AS course, Conditions
-                        FROM Client c
-                        INNER JOIN Seller s
-                        ON c.USERID = s.USERID
-                        INNER JOIN Copy co
-                        ON s.SELLERID = co.SELLERID
-                        INNER JOIN Book b
-                        ON co.BOOKID = b.BOOKID
-                        INNER JOIN Classes cl
-                        ON cl.CLASSID = b.CLASSID;" ;
+
+                    if (empty($_GET)) {
+                        $query = " SELECT clientName, Title, Author, Edition,(COALESCE(cl.subjectname, '') || COALESCE(cl.subjectnum, '')) AS course, Conditions
+                            FROM Client c
+                            INNER JOIN Seller s
+                            ON c.USERID = s.USERID
+                            INNER JOIN Copy co
+                            ON s.SELLERID = co.SELLERID
+                            INNER JOIN Book b
+                            ON co.BOOKID = b.BOOKID
+                            INNER JOIN Classes cl
+                            ON cl.CLASSID = b.CLASSID;" ;
+                    } else {
+                        $input = $_GET["search_query"];
+                    }
 
                     $result = mysqli_query($db, $query);
                     $counter=1;
