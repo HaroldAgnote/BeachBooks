d0ccef522f1e43c92aac642276c8f902d0ec673f
Harold Agnote <haroldagnote38@hotmail.com>
2017-11-15 17:16:54 -0800

    Updated select books.sql

--- database/select_books.sql
+++ database/select_books.sql
@@ -1,6 +1,4 @@
-SELECT c.ClientName, b.Title, b.Author, b.Edition,(COALESCE(cl.SubjectName, '') || COALESCE(cl.SubjectNum, '')) AS Class,
- co.Conditions
-FROM Client c
+SELECT c.ClientName, b.Title, b.Author, b.Edition, cl.ClassNameNum, co.Conditions FROM Client c
 INNER JOIN Seller s
 ON c.UserID = s.UserID
 INNER JOIN Copy co
