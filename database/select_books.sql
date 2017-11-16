95b6095574f6e3142f21628b69f5fd42cdf2a037
Aimee <Aimee-T@users.noreply.github.com>
2017-11-15 16:10:30 -0800

    Update select_books.sql

--- database/select_books.sql
+++ database/select_books.sql
@@ -8,3 +8,17 @@ INNER JOIN Book b
 ON co.BOOKID = b.BOOKID
 INNER JOIN Classes cl
 ON cl.CLASSID = b.CLASSID;
+
+--ALTERNATE
+SELECT c.clientName, b.Title, b.Author, b.Edition,(COALESCE(cl.subjectname, '') || COALESCE(cl.subjectnum, '')) AS Classs,
+ co.Conditions
+FROM Client c
+INNER JOIN Seller s
+ON c.USERID = s.USERID
+INNER JOIN copy co
+ON s.SELLERID = co.SELLERID
+INNER JOIN book b
+ON co.BOOKID = b.BOOKID
+INNER JOIN classes cl
+ON cl.CLASSID = b.CLASSID;
+
