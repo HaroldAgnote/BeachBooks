4e065b5dd96e28b0b641ff57e7db717a32cb102c
Harold Agnote <haroldagnote38@hotmail.com>
2017-11-18 11:16:05 -0800

    Fixed stuff

--- home.php
+++ home.php
@@ -22,7 +22,7 @@
             if (!$db)
             {
                 echo "Error: Unable to connect to MySQL." . PHP_EOL;
-                echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+                echo "Debugging error: " . mysqli_connect_errno() . PHP_EOL;
                 echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
                 exit;
             }
