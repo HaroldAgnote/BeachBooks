18377936e3d973d0901f301d3a431a9ddcdf1b52
Harold Agnote <haroldagnote38@hotmail.com>
2017-11-27 19:35:57 -0800

    Updating home page php code

--- home.php
+++ home.php
@@ -45,15 +45,19 @@
                 <?php 
 
                     if (empty($_GET)) {
-                        $query = " SELECT c.ClientName, co.BookCover, b.Title, b.Author, b.Edition, cl.ClassNameNum, co.Conditions FROM Client c
-                            INNER JOIN Seller s
-                            ON c.UserID = s.UserID
-                            INNER JOIN Copy co
-                            ON s.SellerID = co.SellerID
-                            INNER JOIN Book b
-                            ON co.BookID = b.BookID
-                            INNER JOIN Classes cl
-                            ON cl.ClassID = b.ClassID; " ;
+                        $query = " SELECT c.ClientName, b.Title, b.Author, b.Edition, cl.ClassSubjNum, co.Conditions, bc.URL
+                                FROM Client c
+                                INNER JOIN Seller s
+                                ON c.UserID = s.UserID
+                                INNER JOIN Copy co
+                                ON s.SellerID = co.SellerID
+                                INNER JOIN Book b
+                                ON co.BookID = b.BookID
+                                INNER JOIN Classes cl
+                                ON cl.BookID = b.BookID
+                                INNER JOIN BookCover bc
+                                ON co.COPYID = bc.COPYID;
+                                " ;
                     } else {
                         $input = $_GET["search_query"];
                     }
@@ -62,11 +66,11 @@
                     $counter=1;
                     while($row = mysqli_fetch_assoc($result)) {
                         $seller = $row['ClientName'];
-                        $cover = $row['BookCover'];
+                        $cover = $row['URL'];
                         $title = $row['Title'];
                         $author = $row['Author'];
                         $edition = $row['Edition'];
-                        $class = $row['ClassNameNum'];
+                        $class = $row['ClassSubjNum'];
                         $condition = $row['Conditions'];
                         $counter = $counter + 1;
                 ?>
